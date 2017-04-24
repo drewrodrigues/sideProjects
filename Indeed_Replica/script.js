1073365e3a59dad875f8531ca2161f3b9a46667a
Andrew Rodrigues <rodandrew95@gmail.com>
2017-04-23 22:42:12 -0700

    update styles and js

--- Indeed_Replica/script.js
+++ Indeed_Replica/script.js
@@ -1,16 +1,16 @@
 // TODO: fix view for different phones
-// TODO: test on a tablet in different views
 // TODO: clean up code
-// TODO: change text to Show Less once drawer is open
-// TODO: animate hamburger icon to an x, and change color to white
 $(function() {
   $overlay = $('#overlay');
   $nav = $('nav');
   $footerLinks = $('.footer-links li').not(':first');
-  
+  $linkShowMore = $('.link-showmore');
+  // this has the text for the lower drawer button
+  $linkSub = $('.link-sub').first();
+
   var animateDrawer = function( drawer, toClose ) {
     toClose.removeClass('open').slideUp();
-    
+
     if ( drawer.hasClass('open') ) {
       drawer.slideUp()
             .removeClass('open');
@@ -21,20 +21,25 @@ $(function() {
       $overlay.fadeIn();
     }
   };
-  
+
   var closeDrawers = function() {
     $('.open').slideUp().removeClass('open');
     $overlay.fadeOut();
   };
-  
+
   $('.ham-icon').on('click', function() {
     animateDrawer( $nav, $footerLinks );
   });
-  
+
   $('.link-showmore').on('click', function() {
     animateDrawer( $footerLinks, $nav )
+    if ( $footerLinks.hasClass('open') ) {
+        $linkSub.text('Show Less');
+    } else {
+        $linkSub.text('Show More');
+    }
   });
-  
+
   $('#overlay').on('click', function() {
     closeDrawers();
   });
